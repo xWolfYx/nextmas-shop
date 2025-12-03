5bf3596a20a70f30a247fe681fa58b1b99933d9f
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-03 17:53:20 +0400

    feat: add allura and montseratt fonts

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Allura, Montserrat } from "next/font/google";
+
+export const allura = Allura({ subsets: ["latin"], weight: "400" });
+export const montseratt = Montserrat({ subsets: ["latin"] });
