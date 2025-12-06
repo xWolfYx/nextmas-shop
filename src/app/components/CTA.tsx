57a4f49bb6c92a42127f096d6c27f8f1ad8c9edb
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-07 00:09:11 +0400

    refactor: rewrite to client component

--- src/app/components/CTA.tsx
+++ src/app/components/CTA.tsx
@@ -1,22 +1,28 @@
+"use client";
+
+import dynamic from "next/dynamic";
 import DarkButton from "../ui/DarkButton";
 import { allura } from "../ui/fonts";
-import Timer from "../ui/Timer";
+
+const Timer = dynamic(() => import("../ui/Timer"), { ssr: false });
 
 export default function CTA() {
   return (
-    <section className="flex flex-col items-center justify-center gap-5 rounded-b-[20px] bg-[#ff4646] bg-[url(/backgrounds/bg-snow.png),url(/backgrounds/bg-forest.png)] bg-cover bg-center py-[120px]">
-      <h2 className="text-center text-2xl font-semibold tracking-[0.24rem] text-white uppercase">
+    <section className="flex flex-col items-center justify-center gap-5 rounded-b-[20px] bg-[#ff4646] bg-[url(/backgrounds/bg-snow.png),url(/backgrounds/bg-forest.png)] bg-cover bg-center py-[180px]">
+      <h2 className="text-center text-2xl/9 font-semibold tracking-[0.24rem] text-white uppercase">
         Ready to start your <br />
         journey to a better <br />
         version of yourself?
       </h2>
       <DarkButton text="Explore Magical Gifts" />
-      <p
-        className={`${allura.className} text-center text-2xl tracking-[0.075rem] text-white`}
-      >
-        The New Year is Coming Soon...
-      </p>
-      <Timer />
+      <div className="flex flex-col gap-2">
+        <p
+          className={`${allura.className} text-center text-2xl tracking-[0.075rem] text-white`}
+        >
+          The New Year is Coming Soon...
+        </p>
+        <Timer />
+      </div>
     </section>
   );
 }
