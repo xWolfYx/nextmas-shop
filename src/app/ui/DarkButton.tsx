70a30f93980ae2b52078b3ebd373c35fd86bda97
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-07 00:03:56 +0400

    feat: add link to the gift page

--- src/app/ui/DarkButton.tsx
+++ src/app/ui/DarkButton.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 interface DarkButtonProps {
   text: string;
 }
 
 export default function DarkButton({ text }: DarkButtonProps) {
   return (
-    <button className="cursor-pointer rounded-[20px] bg-[#191c29] px-8 py-5 text-[12px] font-semibold tracking-[0.037rem] text-white uppercase duration-400 hover:bg-white hover:text-[#191c29]">
-      {text}
+    <button className="cursor-pointer rounded-[20px] bg-[#191c29] text-[12px] font-semibold tracking-[0.037rem] text-white uppercase duration-400 hover:bg-white hover:text-[#191c29]">
+      <Link href="/gifts" className="block px-8 py-5">
+        {text}
+      </Link>
     </button>
   );
 }
