9f9cbd911c209cbf90579aa8c4365ba3d9f6fdbd
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-06 15:41:41 +0400

    refactor: change letter spacing to match the design

--- src/app/ui/DarkButton.tsx
+++ src/app/ui/DarkButton.tsx
@@ -4,7 +4,7 @@ interface DarkButtonProps {
 
 export default function DarkButton({ text }: DarkButtonProps) {
   return (
-    <button className="max-w-[235px] cursor-pointer rounded-[20px] bg-[#191c29] px-8 py-5 text-[12px] font-semibold uppercase duration-400 hover:bg-white hover:text-[#191c29]">
+    <button className="cursor-pointer rounded-[20px] bg-[#191c29] px-8 py-5 text-[12px] font-semibold tracking-[0.037rem] text-white uppercase duration-400 hover:bg-white hover:text-[#191c29]">
       {text}
     </button>
   );
