b429db3818b8a8aaba51b1417654ea3440974eeb
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-06 15:43:15 +0400

    refactor: change spacing to match the design

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,13 +3,19 @@ import { allura } from "@/app/ui/fonts";
 
 export default function Hero() {
   return (
-    <section className="flex h-[524px] flex-col items-center justify-center rounded-t-[20px] bg-[#ff4646] bg-[url(/backgrounds/bg-snow.png),url(/backgrounds/bg-ball.png)] bg-cover bg-center *:text-white">
-      <p className={`${allura.className} text-[1.5rem]`}>Merry Christmas</p>
-      <h1 className="w-[426px] text-center text-[2rem] font-semibold uppercase">
-        Gift yourself the magic of new possibilities
+    <section className="flex h-[524px] gap-3 flex-col items-center justify-center rounded-t-[20px] bg-[#ff4646] bg-[url(/backgrounds/bg-snow.png),url(/backgrounds/bg-ball.png)] bg-cover bg-center *:text-white">
+      <p className={`${allura.className} text-[1.5rem] tracking-[0.075rem]`}>
+        Merry Christmas
+      </p>
+      <h1 className="text-center text-[2rem] font-semibold tracking-[0.36rem] uppercase">
+        Gift yourself
+        <br /> the magic of new
+        <br /> possibilities
       </h1>
       <DarkButton text={"Explore Magical Gifts"} />
-      <p className={`${allura.className} text-[1.5rem]`}>and Happy New Year</p>
+      <p className={`${allura.className} text-[1.5rem] tracking-[0.075rem]`}>
+        and Happy New Year
+      </p>
     </section>
   );
 }
