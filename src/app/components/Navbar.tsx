7099b23681d04db8e22fac20c10ed6c0fe25dc06
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-07 00:11:18 +0400

    feat: add anchors to sections and link to the gifts page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between px-2 py-3 md:px-[82px]">
-      <div className="flex cursor-pointer gap-2 px-5 py-2 *:content-center">
+    <nav className="flex justify-between items-center px-2 py-3 md:px-[82px]">
+      <Link
+        href="/"
+        className="flex cursor-pointer gap-2 px-5 py-2 *:content-center"
+      >
         <Image
           src="/snowflake-logo.png"
           width={96}
@@ -14,12 +18,40 @@ export default function Navbar() {
         <span className="text-[0.75rem] font-semibold tracking-[0.037rem] uppercase">
           The Gifts
         </span>
-      </div>
-      <ul className="hidden gap-2 font-semibold tracking-[0.037rem] uppercase *:cursor-pointer *:content-center *:rounded-xl *:px-5 *:py-3 *:text-[0.75rem] *:duration-400 *:hover:bg-[#ff46461a] *:hover:text-[#ff4646] md:flex">
-        <li>Gifts</li>
-        <li>About</li>
-        <li>Best</li>
-        <li>Contacts</li>
+      </Link>
+      <ul className="hidden gap-2 font-semibold tracking-[0.037rem] uppercase md:flex">
+        <li>
+          <Link
+            href="/gifts"
+            className="cursor-pointer content-center rounded-xl px-5 py-3 text-[0.75rem] duration-400 hover:bg-[#ff46461a] hover:text-[#ff4646]"
+          >
+            Gifts
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/#about"
+            className="cursor-pointer content-center rounded-xl px-5 py-3 text-[0.75rem] duration-400 hover:bg-[#ff46461a] hover:text-[#ff4646]"
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/#best"
+            className="cursor-pointer content-center rounded-xl px-5 py-3 text-[0.75rem] duration-400 hover:bg-[#ff46461a] hover:text-[#ff4646]"
+          >
+            Best
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#contacts"
+            className="cursor-pointer content-center rounded-xl px-5 py-3 text-[0.75rem] duration-400 hover:bg-[#ff46461a] hover:text-[#ff4646]"
+          >
+            Contacts
+          </Link>
+        </li>
       </ul>
     </nav>
   );
