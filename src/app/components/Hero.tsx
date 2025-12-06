9390392b1ba7e6de0d3edc32d9fde37bdaf0b983
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-07 00:10:04 +0400

    refactor: minor formatting

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,7 @@ import { allura } from "@/app/ui/fonts";
 
 export default function Hero() {
   return (
-    <section className="flex h-[524px] gap-3 flex-col items-center justify-center rounded-t-[20px] bg-[#ff4646] bg-[url(/backgrounds/bg-snow.png),url(/backgrounds/bg-ball.png)] bg-cover bg-center *:text-white">
+    <section className="flex h-[524px] flex-col items-center justify-center gap-3 rounded-t-[20px] bg-[#ff4646] bg-[url(/backgrounds/bg-snow.png),url(/backgrounds/bg-ball.png)] bg-cover bg-center *:text-white">
       <p className={`${allura.className} text-[1.5rem] tracking-[0.075rem]`}>
         Merry Christmas
       </p>
