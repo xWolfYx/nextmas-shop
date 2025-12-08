fb163bb96a4eb18b08296bb277d338192d3bd171
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-08 17:09:52 +0400

    refactor: remove header and footer components

--- src/app/gifts/page.tsx
+++ src/app/gifts/page.tsx
@@ -1,11 +1,9 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Gifts from "../ui/Gifts";
 
 export default function Page() {
   return (
     <div className="m-auto max-w-360">
-      <Header />
-      <Footer />
+      <Gifts />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 import About from "./components/About";
 import CTA from "./components/CTA";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Slider from "./components/Slider";
 
 export default function Page() {
   return (
-    <div className="m-auto max-w-360">
-      <Header />
+    <>
       <Hero />
       <About />
       <Slider />
       <CTA />
-      <Footer />
-    </div>
+    </>
   );
 }
