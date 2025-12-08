3fc806b1e34d10e12f5946e9b536b4d4caec7dca
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-08 17:47:26 +0400

    feat: add active page link state to navigation

--- src/app/ui/ListItems.tsx
+++ src/app/ui/ListItems.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Link from "next/link";
+import { clsx } from "clsx";
+import { usePathname } from "next/navigation";
 
 type ListLinks = {
   text: string;
@@ -19,11 +23,18 @@ export default function ListItems({
   classList: string;
   callback?: () => void;
 }) {
+  const pathName = usePathname();
   return (
     <>
       {listLinks.map((link) => (
         <li key={link.href} onClick={callback}>
-          <Link href={link.href} className={classList}>
+          <Link
+            href={link.href}
+            className={clsx(`${classList}`, {
+              "pointer-events-none bg-[#ff46461a] text-[#ff4646]":
+                pathName === link.href,
+            })}
+          >
             {link.text}
           </Link>
         </li>
