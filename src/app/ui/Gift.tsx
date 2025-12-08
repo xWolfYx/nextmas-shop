f1f625ed87ee8f506486f4ed3f01a705f9c873f8
xWolfYx <david.skliarov.dev@gmail.com>
2025-12-08 17:10:51 +0400

    feat: add gift component

---
+++ src/app/ui/Gift.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+export default function Gift({
+  name,
+  category,
+}: {
+  name: string;
+  category: string;
+}) {
+  let categorySrc = "";
+  let categoryColor = "";
+
+  if (category === "For Work") {
+    categorySrc = "/gifts/gift-for-harmony.png";
+    categoryColor = "#4361ff";
+  }
+
+  if (category === "For Health") {
+    categorySrc = "/gifts/gift-for-health.png";
+    categoryColor = "#06a44f";
+  }
+
+  if (category === "For Harmony") {
+    categorySrc = "/gifts/gift-for-harmony.png";
+    categoryColor = "#ff43f7";
+  }
+
+  return (
+    <div className="max-w-[310px] rounded-[1.25rem] bg-[#ecf3f8]">
+      <div className="rounded-[1.25rem]">
+        <Image
+          src={categorySrc}
+          width={400}
+          height={400}
+          alt={name}
+          className="h-[230px] w-[310px]"
+        />
+      </div>
+      <div className="flex h-[112] flex-col gap-2 rounded-[1.25rem] bg-white p-5">
+        <span
+          className={`text-[0.75rem] font-semibold tracking-[0.063rem] text-[${categoryColor}] uppercase`}
+        >
+          {category}
+        </span>
+        <span className="font-semibold tracking-[0.08rem] uppercase">
+          {name}
+        </span>
+      </div>
+    </div>
+  );
+}
