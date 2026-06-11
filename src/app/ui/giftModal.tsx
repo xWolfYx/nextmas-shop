565f75b545ac2924b35d99a1e3e9fe9faa6c1c04
WolfY <david.skliarov.dev@gmail.com>
2026-06-11 13:59:06 +0400

    feat: add margin to snowflakes

--- src/app/ui/giftModal.tsx
+++ src/app/ui/giftModal.tsx
@@ -144,7 +144,7 @@ function Snowflakes({ count }: { count: number }) {
 			key={i}
 			width={16}
 			height={16}
-			className={clsx("size-4", i >= count / 100 && "opacity-15")}
+			className={clsx("not-last:mr-2 size-4", i >= count / 100 && "opacity-15")}
 		/>
 	));
 }
